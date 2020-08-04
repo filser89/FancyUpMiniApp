6593a68e0dc1b32944bd0f3c490797ad95c94851
Sergey Filippov <sergey.filippov.89@inbox.ru>
2020-08-04 15:38:14 +0800

    initial commit

---
+++ FancyUpMiniApp/miniprogram/app.js
@@ -0,0 +1,26 @@
+//app.js
+App({
+  onLaunch: function () {
+    const host = 'http://localhost:3000/'
+    console.log('beginning login')
+    wx.login({
+      success: (res) => {
+        console.log(res)
+        // insert next code here
+        wx.request({
+          url: host + 'login',
+          method: 'post',
+          data: {
+            code: res.code
+          },
+          // insert next code here
+          success: (res) => {
+            console.log(res)
+            this.globalData.userId = res.data.userId
+          }
+        })
+      }
+    })
+  },
+  globalData: {}
+})
